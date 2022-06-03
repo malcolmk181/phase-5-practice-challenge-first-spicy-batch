6aa76ab007c009d00e926e39adc02b95f0b8847d
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-03 14:55:04 -0400

    commit tree b3c840a801ac6ccb6f1cd896df802e1291e032fa

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -3,6 +3,25 @@
 # @return {boolean} whether the batch is spicy
 # def is_spicy_batch(batch):
 
+# given an integer n
+# find the first number 0..n that is spicy
+# by calling is_spicy_batch(number)
+
+# my idea
+# start in the middle and check if spicy
+  # if it is spicy, go further left (halfway between original spot and last spot)
+  # if it is not spicy, go further right (halfway between original spot and last spot)
+# keep checking middles until I get to point where boundaries are equal
+
 def first_spicy_batch(n)
   # your code here
+  l_bound = 1
+  r_bound = n
+
+  while l_bound < r_bound
+    target = (r_bound + l_bound) / 2
+    is_spicy_batch(target) ? r_bound = target : l_bound = target + 1
+  end
+
+  return l_bound
 end
